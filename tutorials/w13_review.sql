73d35073f2d93153dafc3a7f4b0819382c5a4f5d
jamesh14 <james.inbox.inbox@gmail.com>
2025-11-07 14:31:14 +1100

    Update w13_review.sql

--- tutorials/w13_review.sql
+++ tutorials/w13_review.sql
@@ -1,5 +1,11 @@
 /*Question 2
 Suppose you are given a relation R with four attributes ABCD. For the given sets of FDs,
 A → B, BC → D, A → C
-
+a) identify the canidate key(s) for R
+attribute closure
+R = (A,B,C,D)
+A -> B, closure = {A, B}
+A -> C, closure = {A, B, C}
+BC -> D, closure = {A, B, C, D}
+{A}+ = {A, B, C, D}
 
