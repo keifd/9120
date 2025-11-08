534876008c6a1afff63fe2bf756adf772d504c19
jamesh14 <james.inbox.inbox@gmail.com>
2025-11-08 17:32:50 +1100

    Update w13_review.sql

--- tutorials/w13_review.sql
+++ tutorials/w13_review.sql
@@ -8,4 +8,10 @@ A -> B, closure = {A, B}
 A -> C, closure = {A, B, C}
 BC -> D, closure = {A, B, C, D}
 {A}+ = {A, B, C, D}
-
+A is the canidate key
+b) 1NF each cell has single value
+2NF all non-key attributes must depend on the entire canidate key
+3NF either the left is superkey, or the right is part of canidate key
+BC is not superkey, thus it violates 3NF
+c) R1(B,C,D)
+R2(A, B, C)
